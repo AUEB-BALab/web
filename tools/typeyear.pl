cdb074bcc2ade7ecc653c0bf6b95aabdd99402cb
Vassilios Karakoidas <vassilios.karakoidas@gmail.com>
2008-03-17 13:48:09 +0000

    added script for categorizing the pubs per year

---
+++ tools/typeyear.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+#
+# $Id$
+#
+# Split publications by year
+#
+
+# Associate the publication year with each publication
+
+$#ARGV == 0 || die "Usage: $0 file.bib\n";
+open(IN, $ARGV[0]) || die "Unable to open $ARGV[0]: $!\n";
+while (<IN>) {
+	next if (/^\@string/i);
+	if(/^\@\w+\s*\{(\w+)/) {
+		endrecord() if (defined($key));
+		$key = lc($1);
+	} elsif (/^\s*year\s*\=\D*(\d+)/i) {
+		print STDERR "Undefined key on line $.\n" unless (defined($key));
+		print STDERR "Duplicate year on line $.\n" if (defined($year));
+		$year = $1;
+	}
+}
+endrecord();
+
+sub
+endrecord
+{
+	print STDERR "Undefined key on line $.\n" unless (defined($key));
+	print STDERR "Undefined year on line $.\n" unless (defined($year));
+	print "$year $key\n";
+
+	undef $key;
+	undef $year;
+}
