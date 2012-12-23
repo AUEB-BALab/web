acbfa7550c0c52a60a7547cd776f7e526d284706
Diomidis Spinellis <dds@aueb.gr>
2012-12-23 10:52:08 +0200

    Avoid spurious rebuilds
    git fetch can create stderr output if it fetches stuff, but also
    if it fails.  Check the git error code and silently abort if git
    has failed.

--- update.sh
+++ update.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
+#
+# Executed periodically by istlab:iweb to update the web page
+#
 
 export PATH=/usr/local/bin:$PATH
 
 cd web
 
 # Fetch updates and see if something was fetched
-git fetch 2>fetch.out
+git fetch 2>fetch.out || exit 1
+
 if ! [ -s fetch.out ]
 then
 	exit 0
 fi
+
+# Show us what has changed
+cat fetch.out
 
 # There is work to integrate; rebuild
 git merge origin
